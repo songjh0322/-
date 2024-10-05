632aa58eb9a6018e24fcc7f30f98460c01462d6e
Songjh0322 <happymine0322@naver.com>
2024-10-05 15:31:18 +0900

    character Move Animation

--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -6,21 +6,48 @@ public class PlayerAction : MonoBehaviour
 {
     float h;
     float v;
-
+    public float speed = 5f;
+    bool isHorizonMove;
     Rigidbody2D rigid;
+    Animator anim;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
+        //Move Value
         h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
+
+        //Check Button Down & Up
+        bool hDown = Input.GetButtonDown("Horizontal");
+        bool vDown = Input.GetButtonDown("Vertical");
+        bool hUp = Input.GetButtonUp("Horizontal");
+        bool vUp = Input.GetButtonUp("Vertical");
+
+        //Check Horizontal Move
+        if (hDown || vUp)
+        {
+            isHorizonMove = true;
+        }
+        else if (vDown || hUp)
+        {
+            isHorizonMove = false;
+        }
+
+        //Animation
+        anim.SetInteger("hAxisRaw", (int)h);
+        anim.SetInteger("vAxisRaw", (int)v);
+
     }
     private void FixedUpdate()
     {
-        rigid.velocity = new Vector2(h * 5, v * 5);
+        //Move
+        Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
+        rigid.velocity = moveVec * speed;
     }
 }
